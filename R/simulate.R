ae85675e23e7cefca8fd9b3db0e9176ec4cc8a04
John Flournoy <uoflournoy@gmail.com>
2019-11-07 17:08:48 -0500

    fixed simulation code

--- R/simulate.R
+++ R/simulate.R
@@ -1,4 +1,4 @@
-#' Title
+#' simulate_mediation_data
 #'
 #' @param J
 #' @param n_j
@@ -62,8 +62,100 @@ simulate_mediation_data <- function(J = 100, n_j = 4, a = 0, b = 0, c_p = 0, the
                         newdata = newdata,
                         newparams = params.y,
                         family = 'gaussian', re.form = NA)[[1]]
+  #Center data within person, and center within-person means
+  x_w_mean <- aggregate(newdata$x, by = list(newdata$id), FUN = mean, na.rm = T)
+  colnames(x_w_mean) <- c("id", "x_w_mean")
+  newdata <- merge(newdata, x_w_mean, by="id")
+  newdata$x_mean <- mean(x_w_mean$x_w_mean)
+  newdata$x_bc <- newdata$x_w_mean - newdata$x_mean
+  newdata$x_wc <- newdata$x - newdata$x_w_mean
+  m_w_mean <- aggregate(newdata$m, by = list(newdata$id), FUN = mean, na.rm = T)
+  colnames(m_w_mean) <- c("id", "m_w_mean")
+  newdata <- merge(newdata, m_w_mean, by="id")
+  newdata$m_mean <- mean(m_w_mean$m_w_mean)
+  newdata$m_bc <- newdata$m_w_mean - newdata$m_mean
+  newdata$m_wc <- newdata$m - newdata$m_w_mean
   return(newdata)
 }
-run_permutation_simulation <- function(nrep, mc.cores, J = 100, n_j = 4, a = 0, b = 0, c_p = 0, theta_ab = .2, optimizer = "bobyqa"){
 
+#' run_permutation_simulation
+#'
+#' @param nrep
+#' @param mc.cores
+#' @param J
+#' @param n_j
+#' @param a
+#' @param b
+#' @param c_p
+#' @param theta_ab
+#' @param optimizer
+#'
+#' @return
+#' @export
+#'
+#' @examples
+run_permutation_simulation <- function(nrep, nperms, mc.cores, J = 100, n_j = 4, a = 0, b = 0, c_p = 0, theta_ab = .2, optimizer = "bobyqa"){
+  #could make this slightly more efficient by splitting up reps differently.
+  reps <- replicate(nrep, {
+    adf <- simulate_mediation_data(J = J, n_j = n_j, a = a, b = b, c_p = c_p, theta_ab = theta_ab)
+    ab <- indirect_within.lme4(data = adf,
+                               indices.y = NULL,
+                               indices.m = NULL,
+                               y.name = 'y',
+                               x.name = 'x_wc',
+                               m_b.name = 'm_wc',
+                               m_a.name = 'm',
+                               group.id = 'id',
+                               covariates.y=c('cov.y', 'm_bc', 'x_bc'),
+                               covariates.m=c('cov.m', 'x_bc'),
+                               random.a=T,
+                               random.b=T,
+                               random.c_p=T, optimizer = optimizer)
+    perms.y <- permute_within(n = nperms, data = adf, group.id = 'id', series = F)
+    perms.m <- permute_within(n = nperms, data = adf, group.id = 'id', series = F)
+    perms_list.y <- lapply(seq_len(nrow(perms.y)), function(i) perms[i,])
+    perms_list.m <- lapply(seq_len(nrow(perms.m)), function(i) perms[i,])
+    split_perms_list.y <- split(perms_list.y, 1:mc.cores)
+    split_perms_list.m <- split(perms_list.m, 1:mc.cores)
+    system.time({
+      ab_perms <- parallel::mcmapply(function(perm_list.y, perm_list.m){
+        mapply(function(indices.y, indices.m){
+          ab <- indirect_within.lme4(data = adf,
+                                     indices.y = indices.y,
+                                     indices.m = indices.m,
+                                     y.name = 'y',
+                                     x.name = 'x_wc',
+                                     m_b.name = 'm_wc',
+                                     m_a.name = 'm',
+                                     group.id = 'id',
+                                     covariates.y=c('cov.y', 'm_bc', 'x_bc'),
+                                     covariates.m=c('cov.m', 'x_bc'),
+                                     random.a=T,
+                                     random.b=T,
+                                     random.c_p=T,
+                                     optimizer = optimizer)
+        }, perm_list.y, perm_list.m, SIMPLIFY = FALSE)
+      }, split_perms_list.y, split_perms_list.m, SIMPLIFY = FALSE, mc.cores = mc.cores)
+    })
+    no_convwarnings <- unlist(lapply(unlist(ab_perms, recursive = F), function(aperm){
+      awarning <- aperm[['warnings']]
+      is.null(awarning)
+    }))
+    ab_vec <- unlist(lapply(unlist(ab_perms, recursive = F), `[[`, 'ab'))
+    ab_vec <- ab_vec[no_convwarnings]
+    ab_vec_centered_mean <- ab_vec - mean(ab_vec)
+    ab_vec_centered_ab <- ab_vec - ab$ab
+    p_ab_perm_mean_c <- ecdf(ab_vec_centered_mean)(ab$ab)
+    p_ab_perm_ab_c <- ecdf(ab_vec_centered_ab)(ab$ab)
+    p_opsign_ci <- mean(sign(ab$ab)*ab_vec < 0)
+    n_conv_warn <- sum(!no_convwarnings)
+    list(p_ab_perm_mean_c = p_ab_perm_mean_c,
+         p_ab_perm_ab_c = p_ab_perm_ab_c,
+         p_opsign_ci = p_opsign_ci,
+         n_conv_warn = n_conv_warn,
+         ab = ab$ab,
+         ab_warnings = ab$warnings,
+         mean_ab_perm = mean(ab_vec))
+  }, simplify = FALSE)
+  return(reps)
 }
---
+++ inst/bin/permediatr_simulation.R
@@ -0,0 +1,67 @@
+library(argparse)
+library(permediatr)
+
+parser <- ArgumentParser(description='Run a simulation study using permediatr')
+parser$add_argument('save_dir', type="character",
+                    help='This is the path where the results will be saved')
+parser$add_argument('simulation_name', type="character", help='Name of the simulation')
+parser$add_argument('--nrep', type="integer",
+                    help = 'Number of repetitions. New data will generated for each repetition.',
+                    required = TRUE)
+parser$add_argument('--nperms', type="integer",
+                    help = 'Number of permutations. This many permutations will be run for each repetition.',
+                    required = TRUE)
+parser$add_argument('--mc.cores', type="integer",
+                    help = 'Number of cores to parallelize over.',
+                    required = TRUE)
+parser$add_argument('--J', type="integer",
+                    help = 'Number of groups.',
+                    required = TRUE)
+parser$add_argument('--n_j', type="integer",
+                    help = 'Number of observations per group.',
+                    required = TRUE)
+parser$add_argument('--a', type="double",
+                    help = 'Coefficient for `a` path (the regression of m on x).',
+                    required = TRUE)
+parser$add_argument('--b', type="double",
+                    help = 'Coefficient for `b` path (the regression of y on m).',
+                    required = TRUE)
+parser$add_argument('--c_p', type="double",
+                    help = 'Coefficient for `c\'` path (the regression of y on x controlling for m).',
+                    required = TRUE)
+parser$add_argument('--theta_ab', type="double",
+                    help = 'Controls the correlation of the random effects of `a` and `b`.',
+                    required = TRUE)
+parser$add_argument('--optimizer', type="character", help='Name of the optimizer to use in the lme4::lmer calls.', default = 'bobyqa')
+args <- parser$parse_args()
+
+args <- parser$parse_args(c(
+  '--nrep', '2',
+  '--nperms', '100',
+  '--mc.cores', '7',
+  '--J', '100',
+  '--n_j', '10',
+  '--a', '0',
+  '--b', '0',
+  '--c_p', '0',
+  '--theta_ab', '.2',
+  '--optimizer', 'bobyqa',
+  '/data/jflournoy/permediatr/',
+  'test'
+))
+
+results <- run_permutation_simulation(nrep = as.numeric(args$nrep),
+                                      nperms = args$nperms,
+                                      mc.cores = args$mc.cores,
+                                      J = args$J,
+                                      n_j = args$n_j,
+                                      a = args$a,
+                                      b = args$b,
+                                      c_p = args$c_p,
+                                      theta_ab = args$theta_ab,
+                                      optimizer = args$optimizer)
+
+saveRDS(results, file = file.path(args$save_dir, paste0(args$simulation_name, '.RDS')))
+results_df <- do.call(rbind,lapply(results, function(x) data.frame(t(unlist(x[-which(names(x) == 'ab_warnings')])))))
+results_csv <- cbind(results_df, data.frame(args))
+write.csv(results_csv, file = file.path(args$save_dir, paste0(args$simulation_name, '.csv')))
