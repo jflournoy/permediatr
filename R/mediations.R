e485e8a17437129929d08b1933f36c42cb6e39a9
John Flournoy <john_flournoy@fas.harvard.edu>
2019-11-11 16:10:26 -0500

    typo

--- R/mediations.R
+++ R/mediations.R
@@ -101,7 +101,7 @@ indirect_within.lme4 <- function(data, indices.y = NULL, indices.m = NULL, y.nam
     }
     #Within-Group Indirect Effects
     within.indirect.effect <- mean(a * b, na.rm=T)
-    singular_fit <- unlist(lapply(e, function(fit) return(ifelse(isSingular(fit), 'Singular', charact))))
+    singular_fit <- unlist(lapply(e, function(fit) return(ifelse(isSingular(fit), 'Singular', character()))))
     warnings <- c(unlist(lapply(e, function(fit) fit@optinfo$warnings)), singular_fit)
   }
   return(list(ab = within.indirect.effect, warnings = warnings))
