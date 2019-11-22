0cd8df7c60bec55707a757afdacdd8c7ebca7b06
John Flournoy <uoflournoy@gmail.com>
2019-11-22 13:51:52 -0500

    extensive rebase

--- R/mediations.R
+++ R/mediations.R
@@ -1,26 +1,24 @@
-#' permediatrMod
+#' lmemm_within
 #'
 #' @param data
-#' @param y.name
-#' @param x.name
-#' @param m_b.name
-#' @param m_a.name
-#' @param group.id
-#' @param covariates.y
-#' @param covariates.m
-#' @param random.a
-#' @param random.b
-#' @param random.c_p
+#' @param y.name y variable name.
+#' @param x.name x variable name (should be within-person centered).
+#' @param m_b.name mediator variable name (within-person centered for computing b path).
+#' @param m_a.name mediator variable name (raw for computing a path).
+#' @param group.id grouping variable for random effects.
+#' @param covariates.y any covariates to include in regression of y.
+#' @param covariates.m any covariates to include in regression of m.
+#' @param random.a include random effect for m on x (a path).
+#' @param random.b include random effect for y on m (b path).
+#' @param random.c_p include random effect for y on x (c' path).
 #' @param optimizer
 #' @param lmeropts
-#' @param re.form.y
-#' @param re.form.m
 #'
 #' @return
 #' @export
-#'
+#' @import lme4
 #' @examples
-permediatrMod <- function(data, y.name, x.name, m_b.name, m_a.name, group.id, covariates.y=NULL, covariates.m=NULL, random.a=T, random.b=T, random.c_p=T, optimizer = "nloptwrap", lmeropts = list()){
+lmemm_within <- function(data, y.name, x.name, m_b.name, m_a.name, group.id, covariates.y=NULL, covariates.m=NULL, random.a=T, random.b=T, random.c_p=T, optimizer = "nloptwrap", lmeropts = list()){
   requireNamespace('lme4', quietly = TRUE)
   re_terms.y <- c('1', c(m_b.name, x.name)[c(random.b, random.c_p)])
   re_terms.m <- c('1', c(x.name)[random.a])
@@ -49,25 +47,81 @@ permediatrMod <- function(data, y.name, x.name, m_b.name, m_a.name, group.id, co
                        c(list(formula = form.m,
                               data = data),
                          lmeropts))
-    list(model.y = model.y,
-         model.m = model.m,
-         lmeropts = lmeropts,
-         data = data,
-         parspec = c(y.name = y.name,
-                     x.name = x.name,
-                     m_b.name = m_b.name,
-                     m_a.name = m_a.name,
-                     group.id = group.id,
-                     covariates.y = covariates.y,
-                     covariates.m = covariates.m,
-                     random.a = random.a,
-                     random.b = random.b,
-                     random.c_p = random.c_p,
-                     optimizer = optimizer,
-                     re.form.y = re.form.y,
-                     re.form.m = re.form.m))
+    r <- list(model.y = model.y,
+              model.m = model.m,
+              lmeropts = lmeropts,
+              data = data,
+              parspec = c(y.name = y.name,
+                          x.name = x.name,
+                          m_b.name = m_b.name,
+                          m_a.name = m_a.name,
+                          group.id = group.id,
+                          covariates.y = covariates.y,
+                          covariates.m = covariates.m,
+                          random.a = random.a,
+                          random.b = random.b,
+                          random.c_p = random.c_p,
+                          optimizer = optimizer))
+    attr(r,'class') <- 'permediatrMod'
+    r
   })
-  attr(e,'class') <- 'permediatrMod'
+  return(e)
+}
+
+#' permediatrPermMod
+#'
+#' @param x a permediatrMod object
+#' @param indices.y indices to use to permute residuals for model.y
+#' @param indices.m indices to use to permute residuals for model.m
+#' @param re.form.y random effects form for y
+#' @param re.form.m random effects form for m
+#'
+#' @return an object of class permediatrPermMod
+#' @export
+#' @import lme4
+#' @examples
+permediatrPermMod <- function(x, indices.y = NULL, indices.m = NULL, re.form.y = NULL, re.form.m = NULL){
+  form.y <- formula(x$model.y)
+  form.m <- formula(x$model.m)
+
+  starFormula.y <- update(form.y, as.formula(paste0('y_star ~ .')))
+  starFormula.m <- update(form.m, as.formula(paste0('m_star ~ .')))
+
+  data <- x$data
+
+  e <- try({
+    residsModel.y <- x$model.y
+    Zy <- predict(residsModel.y, re.form = re.form.y)
+    epsilon_z.y <- residsModel.y@frame$y - Zy
+    P_j.epsilon_z.y <- epsilon_z.y[indices.y]
+    data$y_star <- P_j.epsilon_z.y + Zy
+    model.y <- do.call(lme4::lmer,
+                       c(list(formula = starFormula.y,
+                              data = data),
+                         x$lmeropts))
+    residsModel.m <- x$model.m
+    Zm <- predict(residsModel.m, re.form = re.form.m)
+    epsilon_z.m <- residsModel.m@frame$m - Zm
+    P_j.epsilon_z.m <- epsilon_z.m[indices.m]
+    data$m_star <- P_j.epsilon_z.m + Zm
+    model.m <- do.call(lme4::lmer,
+                       c(list(formula = starFormula.m,
+                              data = data),
+                         x$lmeropts))
+    r <- list(model.y = model.y,
+              model.m = model.m,
+              residsModel.y = residsModel.y,
+              residsModel.m = residsModel.m,
+              lmeropts = x$lmeropts,
+              data = data,
+              parspec = c(x$parspec,
+                          indices.y = indices.y,
+                          indices.m = indices.m,
+                          re.form.y = re.form.y,
+                          re.form.m = re.form.m))
+    attr(r,'class') <- c('permediatrMod', 'permediatrPermMod')
+    r
+  })
   return(e)
 }
 
@@ -83,111 +137,94 @@ is.permediatrMod <- function(x) {
   inherits(x, 'permediatrMod')
 }
 
-#' indirect_within.lme4
+#' is.permediatrPermMod
 #'
-#' Variables should be centered appropriately (i.e., within-person)
+#' @param x
 #'
-#' @param data Data frame. Should not have missing values.
-#' @param indices For permuting. Should be the same length as dim(data)[1]
-#' @param y.name y variable name.
-#' @param x.name x variable name (should be within-person centered).
-#' @param m_b.name mediator variable name (within-person centered for computing b path).
-#' @param m_a.name mediator variable name (raw for computing a path).
-#' @param group.id grouping variable for random effects.
-#' @param covariates.y any covariates to include in regression of y.
-#' @param covariates.m any covariates to include in regression of m.
-#' @param random.a include random effect for m on x (a path).
-#' @param random.b include random effect for y on m (b path).
-#' @param random.c_p include random effect for y on x (c' path).
-#' @param optimizer
-#'
-#' @return indirect and total effect.
-#' @import lme4
+#' @return
 #' @export
-indirect_within.lme4 <- function(x, indices.y = NULL, indices.m = NULL, re.form.y = NULL, re.form.m = NULL, ...) {
+#'
+#' @examples
+is.permediatrPermMod <- function(x) {
+  inherits(x, 'permediatrPermMod')
+}
+
+#' indirectEffect
+#'
+#' Takes a permediatrMod or permediatrPermMod object and extracts the estimate
+#' of the indirect effect.
+#'
+#' @param x a permediatrMod object
+#'
+#' @return an indirectEffect object, with elements ab, the indirect effect
+#'   estiamte, warnings, a vector of warnings, and singular, a vector of
+#'   logicals indicating if convergence was singular.
+#' @export
+#' @import lme4
+#'
+#' @examples
+indirectEffect <- function(x){
   requireNamespace('lme4', quietly = TRUE)
-
-  if(!permediatr::is.permediatrMod(x)){
-    ab_mod <- permediatr::permediatrMod(data = x, ...)
-  } else {
-    ab_mod <- x
-  }
-
-  data <- ab_mod$data
-  group.id <- ab_mod$parspec[['group.id']]
-  x.name <- ab_mod$parspec[['x.name']]
-  m_b.name <- ab_mod$parspec[['m_b.name']]
-
-  form.y <- formula(ab_mod$model.y)
-  form.m <- formula(ab_mod$model.m)
-
-  starFormula.y <- update(form.y, as.formula(paste0('y_star ~ .')))
-  starFormula.m <- update(form.m, as.formula(paste0('m_star ~ .')))
-
-  if(is.null(indices.y) && is.null(indices.m)){
-    #this is not a permutation
-    e <- try({
-      model.y <- ab_mod$model.y
-
-      model.m <- ab_mod$model.m
-      list(model.y, model.m)
-    })
-  } else if(!is.null(indices.y) && !is.null(indices.m)){
-    e <- try({
-      residsModel.y <- ab_mod$model.y
-      Zy <- predict(residsModel.y, re.form = re.form.y)
-      epsilon_z.y <- residsModel.y@frame$y - Zy
-      P_j.epsilon_z.y <- epsilon_z.y[indices.y]
-      data$y_star <- P_j.epsilon_z.y + Zy
-      model.y <- do.call(lme4::lmer,
-                         c(list(formula = starFormula.y,
-                              data = data),
-                           lmeropts))
-      residsModel.m <- ab_mod$model.m
-      Zm <- predict(residsModel.m, re.form = re.form.m)
-      epsilon_z.m <- residsModel.m@frame$m - Zm
-      P_j.epsilon_z.m <- epsilon_z.m[indices.m]
-      data$m_star <- P_j.epsilon_z.m + Zm
-      model.m <- do.call(lme4::lmer,
-                         c(list(formula = starFormula.m,
-                              data = data),
-                           lmeropts))
-      list(model.y, residsModel.y, model.m, residsModel.m)
-    })
-  } else {
-    stop('Indices must be all NULL or given.')
-  }
-  if(inherits(e, 'try-error')){
+  if(inherits(x, 'try-error')){
     within.indirect.effect <- NA
     warnings <- NULL
     singulars <- NULL
-  } else {
-    if(random.a==T){
-      a <- coef(model.m)[[group.id]][[x.name]]
-    } else {
-      a <- fixef(model.m)[x.name]
-    }
-    if(random.b==T){
-      b <- coef(model.y)[[group.id]][[m_b.name]]
-    } else {
-      b <- fixef(model.y)[m_b.name]
-    }
-    #Within-Group Indirect Effects
-    within.indirect.effect <- mean(a * b, na.rm=T)
-    singulars <- unlist(lapply(e, function(fit) {
-      s <- NULL
-      if(isSingular(fit)){
-        s <- TRUE
+    ab_obj <- list(ab = within.indirect.effect, warnings = warnings, singular = singulars)
+  } else if(is.permediatrMod(x)) {
+    ab_obj <- with(as.list(x$parspec), {
+      if(random.a==T){
+        a <- coef(x$model.m)[[group.id]][[x.name]]
+      } else {
+        a <- fixef(x$model.m)[x.name]
       }
-      return(s)
-    }))
-    warnings <- unlist(lapply(e, function(fit) {
-      warnings <- fit@optinfo$warnings
-      return(warnings)
-    }))
+      if(random.b==T){
+        b <- coef(x$model.y)[[group.id]][[m_b.name]]
+      } else {
+        b <- fixef(x$model.y)[m_b.name]
+      }
+      #Within-Group Indirect Effects
+      within.indirect.effect <- mean(a * b, na.rm=T)
+
+      if(is.permediatrPermMod(x)){
+        model_list <- x[c('model.y', 'model.m', 'residsModel.y', 'residsModel.m')]
+      } else {
+        model_list <- x[c('model.y', 'model.m')]
+      }
+      singulars <- unlist(lapply(model_list, function(fit) {
+        s <- NULL
+        if(isSingular(fit)){
+          s <- TRUE
+        }
+        return(s)
+      }))
+      warnings <- unlist(lapply(model_list, function(fit) {
+        warnings <- fit@optinfo$warnings
+        return(warnings)
+      }))
+      list(ab = within.indirect.effect, warnings = warnings, singular = singulars)
+    })
+  } else {
+    stop('Variable "x" has incorrect class: ', class(x))
   }
-  return(list(ab = within.indirect.effect, warnings = warnings, singular = singulars))
+  attr(ab_obj,'class') <- 'indirectEffect'
+  return(ab_obj)
 }
+
+#' lmemm_within.perm
+#'
+#' Variables should be centered appropriately (i.e., within-person)
+#'
+#'
+#' @param ... Passed on to the permediatrPermMod constructor
+#'
+#' @return indirectEffect object.
+#' @export
+lmemm_within.perm <- function(...) {
+  ab_perm_mod <- permediatrPermMod(...)
+  ab <- indirectEffect(ab_perm_mod)
+  return(ab)
+}
+
 #' permute_within
 #'
 #' @param n
